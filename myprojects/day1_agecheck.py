122582ee67007000101e6daaed9dcf2b56f8c71f
Chris Nixon <chris@devnix.io>
2018-10-05 20:36:04 -0500

    attempted to expand app with calcuating days alive and months alive.

--- myprojects/day1_agecheck.py
+++ myprojects/day1_agecheck.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from datetime import timedelta
 from datetime import date
 
+
  # Obtain users birthyear
 def get_birthyear():
     print("Let's see how old you are!:  ")
@@ -15,13 +16,38 @@ def get_birthyear():
 # Get current year
 def get_currentdate():
     currentdate = datetime.today()
-
     return currentdate.year
 
 # Return user age based on previous input!
 
-def main():
+#  print age function
+def current_age():
     age = get_currentdate() - get_birthyear()
     print ("I am guessing you are " + str(age) + " years old!")
+    return age
+
+# calculate and print additional information about age
+
+
+# how may days alive
+def days_old(days):
+    age = current_age()
+    days = int(age) *int(365)
+    print("You have also been alive for " + str(days)  + "!")
+    return days
+
+
+# how many months
+# def months(current_age):
+#    current_age = age
+#    months_old = int(age)  * int(12)
+#  return months_old
+
+# how many minutes, etc
+
+
+def main():
+    current_age()
+    days_old()
 
 main()
